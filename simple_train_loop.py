716fb4922abdb336303169726aa5dbe22a05fe7b
Alex Shtof <alex.shtoff@oath.com>
2021-04-17 14:47:55 +0300

    Commented out proxpt trainer code in simple_train_loop.py

--- simple_train_loop.py
+++ simple_train_loop.py
@@ -19,8 +19,8 @@ fm = FM(num_features, embedding_dim)
 
 dataset = TensorDataset(W_train, y_train)
 epochs_num = 10
-optimizer = torch.optim.Adam(fm.parameters(), lr=3e-4)
-# trainer = ProxPtFMTrainer(fm, step_size)
+# optimizer = torch.optim.Adam(fm.parameters(), lr=3e-4)
+trainer = ProxPtFMTrainer(fm, step_size)
 for epoch in range(epochs_num):
     sum_epoch_loss = 0.
     sum_pred = 0.
@@ -39,26 +39,26 @@ for epoch in range(epochs_num):
             (ignore, w_nz) = torch.nonzero(x_sample, as_tuple=True)
             y = y_sample.squeeze(1)
 
-            optimizer.zero_grad()
-            pred = fm.forward(w_nz.unsqueeze(0))
-            loss = F.binary_cross_entropy_with_logits(pred, y)
-            loss.backward()
-            optimizer.step()
+            # optimizer.zero_grad()
+            # pred = fm.forward(w_nz.unsqueeze(0))
+            # loss = F.binary_cross_entropy_with_logits(pred, y)
+            # loss.backward()
+            # optimizer.step()
+            #
+            # with torch.no_grad():
+            #     sum_epoch_loss += loss.item()
+            #     sum_pred += torch.sigmoid(pred).item()
+            #     sum_label += y.item()
 
             with torch.no_grad():
+                pred = fm.forward(w_nz.unsqueeze(0))
+                loss = F.binary_cross_entropy_with_logits(pred, y)
                 sum_epoch_loss += loss.item()
                 sum_pred += torch.sigmoid(pred).item()
                 sum_label += y.item()
 
-            # with torch.no_grad():
-            #     pred = fm.forward(w_nz.unsqueeze(0))
-            #     loss = F.binary_cross_entropy_with_logits(pred, y)
-            #     sum_epoch_loss += loss.item()
-            #     sum_pred += torch.sigmoid(pred).item()
-            #     sum_label += y.item()
-            #
-            #     y_hat = (2 * y.item() - 1)  # transform 0/1 labels into -1/1
-            #     trainer.step(w_nz, y_hat)
+                y_hat = (2 * y.item() - 1)  # transform 0/1 labels into -1/1
+                trainer.step(w_nz, y_hat)
 
             if (i > 0) and (i % 2000 == 0):
                 update_progress(i)
